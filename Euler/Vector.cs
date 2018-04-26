8f9aeed5b3fa81e004c0aaa3d05aebc64dc70880
Eric J Andow <ericandow@gmail.com>
2018-04-25 23:37:03 -0500

    Tries some data structure practice. Can you tell?

---
+++ Euler/Vector.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    /*
+     * Implements a resizable array of homogenous data.
+    */
+    public class Vector<T> {
+        private T[] arr;
+        private int used;
+        private int capacity;
+
+        public Vector() {
+            capacity = 10;
+            arr = new T[capacity];
+            used = 0;
+        }
+
+        public void Append(T item) {
+            if (used >= capacity) {
+                capacity *= 2;
+                var temp = new T[capacity];
+
+                Array.Copy(arr, temp, arr.Length);
+                arr = temp;
+            }
+            arr[used++] = item;
+            
+        }
+
+        public T Retreive(int index) {
+            if (index < used) {
+                return arr[index];
+            }
+            //else throw exception
+            return default(T);
+        }
+
+        
+        }
+    }
+}
